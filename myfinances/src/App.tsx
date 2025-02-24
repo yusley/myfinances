4a521d1ec3cf8571ed596ef0e44718b34b3ee499
yusley <yusley.santos23@gmail.com>
2025-02-24 00:21:25 -0300

    estrutura do header

--- myfinances/src/App.tsx
+++ myfinances/src/App.tsx
@@ -7,7 +7,22 @@ function App() {
   return (
 
       <div className="w-full h-full">
-        <h1>salve</h1>
+        <section className='px-[2rem] pt-[3rem] pb-[10rem] bg-[var(--header-color)] flex justify-between'>
+          <div className="logo items-center gap-[1rem] flex w-[300px]">
+            <img  src="./public/logo.svg" alt="" />
+            <p className='text-gray-100 text-[20px]'>dt money</p>
+          </div>
+          <div className="actionNewTransaction" >
+         
+              <input
+                className='p-[1rem] bg-[#6933FF] text-gray-50 rounded-[5px] cursor-pointer'
+                type="submit"
+                value="Nova Transação"
+              />
+                     
+          </div>
+        </section>
+        <section className=''></section>
       </div>
   )
 }
